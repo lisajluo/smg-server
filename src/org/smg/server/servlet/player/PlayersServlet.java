e6717c69bfbe8b37f085ff3160d866a3b59af91c
Jinxuan Wu <youjinzhan@gmail.com>
2014-03-20 18:46:09 -0400

    save change before merge

---
+++ src/org/smg/server/servlet/player/PlayersServlet.java
@@ -0,0 +1,24 @@
+package org.smg.server.servlet.player;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Function: Insert a player
+ * Url pattern: /players
+ * @author Archer
+ * 
+ * TODO combine this with /players/{playerId} 
+ */
+@SuppressWarnings("serial")
+public class PlayersServlet extends HttpServlet {
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    
+  }
+  
+}
