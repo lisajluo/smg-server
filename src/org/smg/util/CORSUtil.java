a1232e93315cc1eb407b27057118486c7d5f73c7
Jinxuan Wu <youjinzhan@gmail.com>
2014-03-20 18:07:09 -0400

    test CORS

---
+++ src/org/smg/util/CORSUtil.java
@@ -0,0 +1,11 @@
+package org.smg.util;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSUtil {
+  public static void addCORSHeader(HttpServletResponse resp) {
+    resp.setHeader("Access-Control-Allow-Origin", "*");
+    resp.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    resp.setHeader("Content-Type", "text/csv");
+  }
+}
