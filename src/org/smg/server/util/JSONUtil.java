f6b52b59125f049d993f39fe2ae21dd1ee35a00f
Lisa Luo <lisa.j.luo@gmail.com>
2014-03-22 18:45:52 -0400

    JSONUtil changes

--- src/org/smg/server/util/JSONUtil.java
+++ src/org/smg/server/util/JSONUtil.java
@@ -9,25 +9,20 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONUtil {
-  public static Map<String, Object> parse(String json) {
+  public static Map<String, Object> parse(String json) throws IOException {
     ObjectMapper mapper = new ObjectMapper();    
     // read JSON from a file
     Map<String, Object> map = new HashMap<String,Object>();
-    try {
-      map = mapper.readValue(
-        json,
-        new TypeReference<Map<String, Object>>() {
-      });
-    } catch (JsonParseException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (JsonMappingException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+    
+      try {
+        map = mapper.readValue(
+          json,
+          new TypeReference<Map<String, Object>>() {
+        });
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        throw new IOException("JSON_PARSE_ERROR");
+      }    
      return map;
   }
 }
