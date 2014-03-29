ce4b7de7859b4d107704bc6604ce2407519df0ea
owwlo <owwlo@163.com>
2014-03-28 21:17:12 -0400

    Channel Api support
    Signed-off-by: owwlo <owwlo@163.com>

--- src/org/smg/server/servlet/container/QueueServlet.java
+++ src/org/smg/server/servlet/container/QueueServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.smg.server.database.ContainerDatabaseDriver;
 import org.smg.server.util.CORSUtil;
 import org.smg.server.util.JSONUtil;
 
@@ -16,6 +17,7 @@ import com.google.appengine.api.channel.ChannelService;
 import com.google.appengine.api.channel.ChannelServiceFactory;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
+import com.google.common.collect.ImmutableMap;
 
 public class QueueServlet extends HttpServlet {
 
@@ -82,11 +84,19 @@ public class QueueServlet extends HttpServlet {
       // Token Generating.
       String userId = String.valueOf(playerId);
       ChannelService channelService = ChannelServiceFactory.getChannelService();
-      String token = channelService.createChannel(Utils.getClientId(userId));
+      String channelToken = Utils.getClientId(userId);
+      String clientToken = channelService.createChannel(channelToken);
+
+      Map<String, Object> entityMap = ImmutableMap.<String, Object> of(
+          ContainerConstants.GAME_ID, gameId,
+          ContainerConstants.PLAYER_ID, playerId,
+          ContainerConstants.CHANNEL_TOKEN, channelToken);
+
+      ContainerDatabaseDriver.insertQueueEntity(entityMap);
 
       // Send token back to client.
       try {
-        returnValue.put(ContainerConstants.CHANNEL_TOKEN, token);
+        returnValue.put(ContainerConstants.CHANNEL_TOKEN, clientToken);
       } catch (JSONException e) {
       }
 
