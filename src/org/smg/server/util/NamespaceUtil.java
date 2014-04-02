caa38300f53195740dec1b6a1de89a90f506df75
Lisa Luo <lisa.j.luo@gmail.com>
2014-04-02 19:33:55 -0400

    NamespaceUtil, datastore-indexes changes

--- src/org/smg/server/util/NamespaceUtil.java
+++ src/org/smg/server/util/NamespaceUtil.java
@@ -3,5 +3,9 @@ package org.smg.server.util;
 import com.google.appengine.api.utils.SystemProperty;
 
 public class NamespaceUtil {
-  public static String VERSION = SystemProperty.applicationVersion.get().substring(0, 1);
+  /** 
+   * Form of applicationVersion is <majorVersion>.<minorVersion> where majorVersion is 
+   * the application version specified in appengine-web.xml
+   */
+  public static String VERSION = SystemProperty.applicationVersion.get().split("\\.")[0];
 }
