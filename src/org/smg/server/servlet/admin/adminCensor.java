b5a388cad3b0926b5200266653743107520ef297
owwlo <owwlo@163.com>
2014-04-12 11:55:20 -0400

    adminCensor is working now
    Signed-off-by: owwlo <owwlo@163.com>

--- src/org/smg/server/servlet/admin/adminCensor.java
+++ src/org/smg/server/servlet/admin/adminCensor.java
@@ -1,7 +1,7 @@
+
 package org.smg.server.servlet.admin;
 
 import static org.smg.server.servlet.admin.adminConstants.*;
-
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,129 +22,127 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-
-
-
-
 import org.smg.server.database.GameDatabaseDriver;
 import org.smg.server.database.UserDatabaseDriver;
+import org.smg.server.servlet.container.Utils;
 import org.smg.server.servlet.user.UserUtil;
 import org.smg.server.util.CORSUtil;
 import org.smg.server.util.JSONUtil;
 
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
-public class adminCensor extends HttpServlet{
-	
-    private String[] validParams = {PASS_CENSOR};
-    private void sendEmailHelper(String gameName,long developerId,boolean pass,String Text) throws Exception
-    {
-		try {
-			Map developerInfo = UserDatabaseDriver.getUserMap(developerId);
-			String firstName = (String) developerInfo.get(FIRST_NAME);
-			String emailAddress = (String) developerInfo.get(EMAIL);
-			Properties props = new Properties();
-			Session session = Session.getDefaultInstance(props, null);
-			StringBuffer msgBodyBuffer = new StringBuffer();
-			msgBodyBuffer.append("Hello " + firstName + ":\n");
-			if (pass == true) {
-				msgBodyBuffer.append(approve(gameName));
+public class adminCensor extends HttpServlet {
 
-			} else {
-				msgBodyBuffer.append(disapprove(gameName));
-				msgBodyBuffer.append(Text);
-			}
+  private String[] validParams = {
+    PASS_CENSOR
+  };
 
-			String msgBody = msgBodyBuffer.toString();
+  private void sendEmailHelper(String gameName, long developerId, boolean pass, String Text)
+      throws Exception
+  {
+    try {
+      Map developerInfo = UserDatabaseDriver.getUserMap(developerId);
+      String firstName = (String) developerInfo.get(FIRST_NAME);
+      String emailAddress = (String) developerInfo.get(EMAIL);
+      Properties props = new Properties();
+      Session session = Session.getDefaultInstance(props, null);
+      StringBuffer msgBodyBuffer = new StringBuffer();
+      msgBodyBuffer.append("Hello " + firstName + ":\n");
+      if (pass == true) {
+        msgBodyBuffer.append(approve(gameName));
 
-			Message msg = new MimeMessage(session);
-			msg.setFrom(new InternetAddress(ADMIN_EMAIL, ADMIN_NAME));
-			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
-					emailAddress, firstName));
-			msg.setSubject(MAIL_SUBJECT);
-			msg.setText(msgBody);
-			Transport.send(msg);
-			return;
+      } else {
+        msgBodyBuffer.append(disapprove(gameName));
+        msgBodyBuffer.append(Text);
+      }
 
-		} catch (Exception e) {
-			throw new Exception();
-		}
+      String msgBody = msgBodyBuffer.toString();
+
+      Message msg = new MimeMessage(session);
+      msg.setFrom(new InternetAddress(ADMIN_EMAIL, ADMIN_NAME));
+      msg.addRecipient(Message.RecipientType.TO, new InternetAddress(
+          emailAddress, firstName));
+      msg.setSubject(MAIL_SUBJECT);
+      msg.setText(msgBody);
+      Transport.send(msg);
+      return;
+
+    } catch (Exception e) {
+      throw new Exception();
     }
-    private void sendEmailToDevelopers(long gameId,boolean pass, String Text) throws Exception
-    {
-		try {
-			List<String> developerIdList = GameDatabaseDriver
-					.getDeveloperList(gameId);
-			String gameName = GameDatabaseDriver.getGameName(gameId);
-			for (int i=0;i<developerIdList.size();i++)
-			{
-				Long developerIdLong = Long.parseLong(developerIdList.get(i));
-				sendEmailHelper(gameName,developerIdLong,pass,Text);
-			}
-			
-		} catch (Exception e) {
-			throw new Exception();
-		}
+  }
+
+  private void sendEmailToDevelopers(long gameId, boolean pass, String Text) throws Exception
+  {
+    try {
+      List<String> developerIdList = GameDatabaseDriver
+          .getDeveloperList(gameId);
+      String gameName = GameDatabaseDriver.getGameName(gameId);
+      for (int i = 0; i < developerIdList.size(); i++)
+      {
+        Long developerIdLong = Long.parseLong(developerIdList.get(i));
+        sendEmailHelper(gameName, developerIdLong, pass, Text);
+      }
+
+    } catch (Exception e) {
+      throw new Exception();
     }
-	private Map<Object, Object> deleteInvalid(Map<Object, Object> params,
-		      String[] validParams) {
-		    Map<Object, Object> returnMap = new HashMap<Object, Object>();
-		    for (Map.Entry<Object, Object> entry : params.entrySet()) {
-		      if (Arrays.asList(validParams).contains(entry.getKey())) {
-		        if (entry.getKey() instanceof String) {
-		          returnMap.put(entry.getKey(), entry.getValue());
-		        }
-		      }
-		    }
-		    return returnMap;
-		  }
-	
-	 private void put(JSONObject jObj, String key, String value, HttpServletResponse resp) {
-		    try {
-		      jObj.put(key, value);
-		      resp.setContentType("text/plain");
-		      jObj.write(resp.getWriter());
-		    } catch (Exception e) {
-		      return;
-		    }
-		  }
-	
-	public void doPost(HttpServletRequest req, HttpServletResponse resp)
-		      throws IOException{
-		CORSUtil.addCORSHeader(resp);
-	    JSONObject jObj = new JSONObject();
-	    StringBuffer buffer = new StringBuffer();
-	    String line = null;
-	    BufferedReader reader = req.getReader();
-	    String gameId = req.getParameter(GAME_ID);
-	    try
-	    {
-	      while ((line = reader.readLine()) != null) {
-	        buffer.append(line);
-	      }
-	      Map<Object, Object> parameterMap = deleteInvalid(
-	          (Map) JSONUtil.parse(buffer.toString()), validParams);
-	      if (parameterMap.containsKey(PASS_CENSOR)==false)
-	      {
-	    	  put(jObj, ERROR, MISSING_INFO, resp);
-	          return;
-	      }
-	      parameterMap.put(UPDATED, false);
-	      long longId = Long.parseLong(gameId);
-	      GameDatabaseDriver.updateGame(longId, parameterMap);
-	      put (jObj,SUCCESS,ADMIN_FINISHED,resp);
-	      boolean pass = (boolean)parameterMap.get(PASS_CENSOR);
-	      String text = (String)parameterMap.get(TEXT);
-	      sendEmailToDevelopers(longId,pass,text);
-	      return;
-	    }
-	   catch (Exception e)
-	   {
-		   put(jObj,ERROR,WRONG_GAME_ID,resp);
-		   return;
-	   }
-		
-	}
+  }
+
+  private Map<Object, Object> deleteInvalid(Map<Object, Object> params,
+      String[] validParams) {
+    Map<Object, Object> returnMap = new HashMap<Object, Object>();
+    for (Map.Entry<Object, Object> entry : params.entrySet()) {
+      if (Arrays.asList(validParams).contains(entry.getKey())) {
+        if (entry.getKey() instanceof String) {
+          returnMap.put(entry.getKey(), entry.getValue());
+        }
+      }
+    }
+    return returnMap;
+  }
+
+  private void put(JSONObject jObj, String key, String value, HttpServletResponse resp) {
+    try {
+      jObj.put(key, value);
+      resp.setContentType("text/plain");
+      jObj.write(resp.getWriter());
+    } catch (Exception e) {
+      return;
+    }
+  }
+
+  public void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    CORSUtil.addCORSHeader(resp);
+    JSONObject jObj = new JSONObject();
+    String gameId = req.getPathInfo().substring(1);
+    System.out.println(gameId);
+    try
+    {
+      String buffer = Utils.getBody(req);
+      System.out.println(buffer);
+      Map<Object, Object> parameterMap = deleteInvalid(
+          (Map) JSONUtil.parse(buffer), validParams);
+      if (parameterMap.containsKey(PASS_CENSOR) == false)
+      {
+        put(jObj, ERROR, MISSING_INFO, resp);
+        return;
+      }
+      parameterMap.put(UPDATED, false);
+      long longId = Long.parseLong(gameId);
+      GameDatabaseDriver.updateGame(longId, parameterMap);
+      put(jObj, SUCCESS, ADMIN_FINISHED, resp);
+      boolean pass = (boolean) parameterMap.get(PASS_CENSOR);
+      String text = (String) parameterMap.get(TEXT);
+      sendEmailToDevelopers(longId, pass, text);
+      return;
+    } catch (Exception e)
+    {
+      put(jObj, ERROR, WRONG_GAME_ID, resp);
+      e.printStackTrace();
+      return;
+    }
+
+  }
 }
